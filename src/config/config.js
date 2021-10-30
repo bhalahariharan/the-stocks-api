e9689d06360f7145f46ab4a1580db44107c8c92c
Bhala Hariharan <bhala.vishnu@gmail.com>
2021-10-30 15:08:39 +0530

    Use dburl based on env

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,13 @@
 const config = {
   port: 3001,
-  dbUrl: `mongodb://localhost:27017/the-stocks`,
+  dbUrl: {
+    local: 'mongodb://localhost:27017/the-stocks?retryWrites=true&w=majority',
+    prod: 'mongodb+srv://readOnly:8tB0HC4UWwfWf9wo@the-stocks.zqgao.mongodb.net/the-stocks?retryWrites=true&w=majority',
+  },
 };
+
+export function getDbUrl() {
+  return process.env.NODE_ENV === 'production' ? config.dbUrl.prod : config.dbUrl.local;
+}
 
 export default config;
--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-import config from './config/config.js';
+import config, { getDbUrl } from './config/config.js';
 import routes from './routes/index.js';
 
 const app = express();
 
-mongoose.connect(config.dbUrl).then(() => {
+mongoose.connect(getDbUrl()).then(() => {
+  console.log(`ENV: ${process.env.NODE_ENV || 'development'}`);
   console.log('Connected to Database');
   app.listen(config.port, () => {
     console.log(`App listening at http://localhost:${config.port}`);
